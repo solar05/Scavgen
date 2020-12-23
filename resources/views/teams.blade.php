ec6cb84b89a8217f378914dc9fc81d0b95c1054a
solar05 <artem00298@gmail.com>
2020-12-23 22:02:33 +0300

    Remove white space

--- resources/views/teams.blade.php
+++ resources/views/teams.blade.php
@@ -6,11 +6,8 @@
 
     <title>Scavgen</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <nav class="navbar navbar-light bg-light">
-        <span class="navbar-brand mb-0 h1"></span>
-    </nav>
     </head>
-    <body>
+    <body class="bg-light">
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand h1" href="#">Scavgen</a>
