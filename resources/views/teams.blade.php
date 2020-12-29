1121d7687d912b4ed9fd8f9254b09fc01cf64069
solar05 <artem00298@gmail.com>
2020-12-29 19:17:10 +0300

    Add fonts to team

--- resources/views/teams.blade.php
+++ resources/views/teams.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>Scavgen</title>
+    <link href="./css/app.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     @if (App::environment('production'))
     <script type="text/javascript" >
@@ -24,32 +25,32 @@
     <body class="bg-light">
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand h1" href="#">Scavgen</a>
+            <a class="navbar-brand h1 blended" href="#">Scavgen</a>
             
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
-                    <a class="nav-item nav-link" href="{{ route('single') }}">Собрать дикого</a>
-                    <a class="nav-item nav-link" href="{{ route('team') }}">Собрать отряд</a>
+                    <a class="nav-item nav-link blended-reg" href="{{ route('single') }}">Собрать дикого</a>
+                    <a class="nav-item nav-link blended-reg" href="{{ route('team') }}">Собрать отряд</a>
                 </div>
             </div>
         </nav>
     </div>
     <div class="container border border-dark rounded">
         <div class="jumbotron">
-            <h1 class="display-4 text-center">Генератор отряда диких!</h1>
+            <h1 class="display-4 text-center blended">Генератор отряда диких!</h1>
             <hr class="my-4">
             <div class="border border-dark rounded">
                 @foreach ($namesList as $names)
                     @if ($loop->last)
-                    <h2 class="text-center m-0">{{ $names['fullName'] }}</h2>
+                    <h2 class="text-center m-0 blended-reg-bg">{{ $names['fullName'] }}</h2>
                     @else
-                        <h2 class="text-center m-0 border-bottom border-dark">{{ $names['fullName'] }}</h2>
+                        <h2 class="text-center m-0 border-bottom blended-reg-bg border-dark">{{ $names['fullName'] }}</h2>
                     @endif
                 @endforeach
             </div>
             <br>
             <p class="lead">
-                <a class="btn btn-success btn-lg d-flex justify-content-center" href="{{ route('team') }}" role="button">Сгенерировать отряд диких</a>
+                <a class="btn btn-success btn-lg d-flex justify-content-center blended-reg" href="{{ route('team') }}" role="button">Сгенерировать отряд диких</a>
             </p>
         </div>
     </div>
