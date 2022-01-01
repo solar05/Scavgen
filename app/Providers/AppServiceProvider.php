b6c2b9214d212debe6c616229409acd410a92c68
solar05 <artem00298@gmail.com>
2022-01-02 02:54:19 +0300

    Increase test coverage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,8 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if (Request::server('HTTP_X_FORWARDED_PROTO') == 'https') {
-            URL::forceScheme('https');
-        }
+        // if (Request::server('HTTP_X_FORWARDED_PROTO') == 'https') {
+        //     URL::forceScheme('https');
+        // }
     }
 }
