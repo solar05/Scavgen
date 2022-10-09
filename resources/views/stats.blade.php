21acf96d94e172392c632887553aa98cd332acee
Artem Solomatin <artem00298@gmail.com>
2022-10-09 14:00:34 +0300

    Update stats.blade.php

--- resources/views/stats.blade.php
+++ resources/views/stats.blade.php
@@ -7,6 +7,12 @@
 
     <title>Scavgen</title>
     <link href="./css/app.css" rel="stylesheet">
+    
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+    <link rel="manifest" href="/site.webmanifest">
+    
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     @if (App::environment('production'))
     <script type="text/javascript" >
